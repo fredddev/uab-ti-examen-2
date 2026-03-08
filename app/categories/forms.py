9132b2447647e382e80fc85252d84f39797e98c3
Luis Fernando Guarachi Apaza <luisfernandoguarachia@gmail.com>
2026-03-08 02:45:10 -0400

    Funcion de Eliminar categoria

--- app/categories/forms.py
+++ app/categories/forms.py
@@ -6,3 +6,6 @@ class CategoryForm(FlaskForm):
     name = StringField('Nombre de la Categoría', validators=[DataRequired(), Length(min=1, max=100)])
     description = TextAreaField('Descripción', validators=[Length(max=255)])
     submit = SubmitField('Guardar')
+
+class EmptyForm(FlaskForm):
+    pass
